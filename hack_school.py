78a1f4ff9394a1b34110da86f6c224c6b737a818
Vetreria <90621103+Vetreria@users.noreply.github.com>
2022-12-02 19:40:20 +0300

    Initial commit

---
+++ hack_school.py
@@ -0,0 +1,34 @@
+from django.db import models
+from datacenter.models import (Chastisement, Commendation, Lesson, Mark, Schoolkid, Subject)
+
+
+def name_check():
+    child = input("Введите ФИО ученика: ")
+    try:
+        child_names = Schoolkid.objects.get(full_name__contains=child)
+        print(child_names)
+        return child_names
+    except Schoolkid.DoesNotExist:
+        print("Такого ученика нет")
+    except Schoolkid.MultipleObjectsReturned:
+        print('Или ничего не ввели или много совпадений, уточните имя')
+
+
+def fix_marks():
+    bed_points = Mark.objects.filter(schoolkid=name_check(), points__in=[2, 3])
+    for bed_point in bed_points:
+        bed_point.points = 5
+        bed_point.save()
+
+
+def remove_chastisements():
+    teacher_texts=Chastisement.objects.filter(schoolkid=name_check())
+    for teacher_text in teacher_texts:
+        teacher_text.delete()
+
+
+def create_commendation():
+    schoolkid = name_check()
+    target_lesson = input("Введите предмет: ")
+    hack_lesson = Lesson.objects.filter(year_of_study=schoolkid.year_of_study, group_letter=schoolkid.group_letter[0], subject__title=target_lesson)[0]
+    Commendation.objects.create(text='Молодец!', created=hack_lesson.date, schoolkid=schoolkid, subject =hack_lesson.subject, teacher=hack_lesson.teacher)
